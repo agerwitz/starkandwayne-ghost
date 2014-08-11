2b6c0c159c9878aca78f4e82aa4f3f2fc435f5ae
Long Nguyen <long.nguyen11288@gmail.com>
2014-08-11 18:07:46 -0400

    Upgraded to 0.5.0

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ if (production) {
   var cfEnv = require("cf-env");
   var pkg   = require("./package.json");
   var cfCore = cfEnv.getCore({name: pkg.name});
-  var sqlCredentials = cfEnv.getService("drnic-ghost-pg").credentials;
+  var sqlCredentials = cfEnv.getService("ghost-pg-prod").credentials;
   var mailCredentials = cfEnv.getService("shared-mail").credentials;
 }
 console.log(sqlCredentials);
